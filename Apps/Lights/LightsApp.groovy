80b4b2d495075d8b98904f9b151efecc6ff69c83
Tim Brpown <gikjll@gmail.com>
2025-12-04 06:31:29 -0500

    Add Apache 2.0 copyright headers to Hubitat apps and update coding standards

--- Apps/Lights/LightsApp.groovy
+++ Apps/Lights/LightsApp.groovy
@@ -1,7 +1,7 @@
 /**
  *  Lights App
  *
- *  Copyright 2025 Hubitat
+ *  Copyright 2025 Tim Brown
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
