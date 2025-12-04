60657a53bffdb6a80cd95fa5b1814fe3da68b2b1
Tim Brpown <gikjll@gmail.com>
2025-12-04 06:33:52 -0500

    Standardize copyright headers in CarPortControl and ArriveGraceTurnsOn

--- Apps/CarportBeam/CarPortControl.groovy
+++ Apps/CarportBeam/CarPortControl.groovy
@@ -1,7 +1,7 @@
 /**
  *  CarPort Control
  *
- *  Copyright 2025 Hubitat
+ *  Copyright 2025 Tim Brown
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
